627d024ca3478eae0400639c128bb9a8df48402f
Aashutosh Shrivastav <aashutosh.shrivastav@finastra.com>
2023-03-20 00:34:10 +0530

    feature_hero_page inital commit and theme toggle changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { CoreConstants } from 'src/assets/constants/CoreConstants';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'Portfolio app';
+export class AppComponent implements OnInit,OnDestroy{
+  readonly title = 'Portfolio app';
+  readonly brightTheme:string = CoreConstants.THEME_BRIGHT;
+  readonly darkTheme:string = CoreConstants.THEME_DARK;
+  theme: string = this.brightTheme;
+  constructor(){}
+  ngOnInit(): void {
+  }
+  ngOnDestroy(): void {
+
+  }
+  toggleTheme()
+  {
+    if(this.theme === CoreConstants.THEME_BRIGHT)
+    {
+      this.theme = CoreConstants.THEME_DARK;
+    }
+    else
+    {
+      this.theme = CoreConstants.THEME_BRIGHT;
+    }
+  }
+
 }
