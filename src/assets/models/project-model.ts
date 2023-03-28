f211231c5330beca943b7b4c9f1247f432c0520c
Aashutosh Shrivastav <aashutosh.shrivastav@finastra.com>
2023-03-28 23:56:56 +0530

    feature_project_page initial projects card display logic

---
+++ src/assets/models/project-model.ts
@@ -0,0 +1,10 @@
+export interface Project {
+  title :string;
+  description:string;
+  img?:string;
+  codeLink?:string;
+  demoLink?:string;
+  otherResourceLink?:string;
+  rowSpan:number;
+  colSpan:number;
+}
